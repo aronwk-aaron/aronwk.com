551bee5c07e9d6c62c2ab5e07daa30b86fe713d2
Aaron Kimbrell <aronwk.aaron@gmail.com>
2018-07-08 16:37:38 -0500

    Formatting on Home
    as well as "Under Construction"

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="home">
     <b-container class="mt-5">
-      <b-jumbotron
+      <b-jumbotron class="mb-0"
         header="Aaron Kimbrell"
         lead="Software Engineering Undergradate | Mississippi State University" >
+        <hr class="my-4">
+        <h1>Under Construction</h1>
       </b-jumbotron>
     </b-container>
   </div>
