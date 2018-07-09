c8ef295303f892500baac30b013f8cba4062f400
Aaron Kimbrell <aronwk.aaron@gmail.com>
2018-07-08 19:52:13 -0500

    deploy script update
    forgot to remove the existing html folder
    and have to make the new files readable by httpd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,8 @@
 
 cd aronwk.com
 mv dist html
+sudo rm -rf /var/www/aronwk.com/html
 sudo mv html /var/www/aronwk.com/
 sudo chown -R nginx:nginx /var/www/aronwk.com/html
+sudo chcon -Rt httpd_sys_content_t /var/www/
+
