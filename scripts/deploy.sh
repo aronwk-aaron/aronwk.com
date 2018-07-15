e88a6286966a5a7618f6f7525229fee9c2fac2af
Aaron Kimbrell <aronwk.aaron@gmail.com>
2018-07-14 19:59:07 -0500

    updated deploy script with recursive copy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@
 cd ~/aronwk.com
 mv dist html
 sudo rm -rf /var/www/aronwk.com/html
-sudo cp html /var/www/aronwk.com/
+sudo cp -r html /var/www/aronwk.com/
 sudo chown -R $USER:$USER /var/www/aronwk.com/html
